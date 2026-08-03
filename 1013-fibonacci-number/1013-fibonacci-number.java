c0bcd4a144c6af59744c46f3ae9e24076c71be2f
Aakash Chandra <142911902+Aakash50909@users.noreply.github.com>
2026-08-03 23:27:50 +0530

    1013-FIBONACCI-NUMBER Stats: Time: 0 ms (100%), Space: 41.9 MB (86.03%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -12,10 +12,7 @@ class Solution
         if(n==1)
         return 1;
         int dp[]= new int[n+1];
-        for(int i=0;i<n;i++)
-        {
-            dp[i]=0;
-        }
+    
         dp[0]=0;
         dp[1]=1;
         for(int i=2;i<=n;i++)
