a3cdf7f44a059b0474cbe07b334e91d37b6d0fe8
Aakash Chandra <142911902+Aakash50909@users.noreply.github.com>
2026-08-03 22:57:33 +0530

    1013-FIBONACCI-NUMBER Stats: Time: 0 ms (100%), Space: 42.1 MB (52.98%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -3,15 +3,18 @@ class Solution {
     ArrayList <Integer> array= new ArrayList<>();
     public int fib(int n)
     {
-        array.addAll(Collections.nCopies(n, -1));
+        array.addAll(Collections.nCopies(n+1, -1));
         return fibDP(n,array);
     }
     public int fibDP(int n, ArrayList <Integer> arr) 
     {
         if(n<=1)
         return n;
-        if(arr.get(n-1)!=-1)
-        return arr.get(n-1);
-        return fibDP(n-1,arr)+fibDP(n-2,arr);
+        if(arr.get(n)!=-1)
+        {
+            return arr.get(n);
+        }
+        arr.set(n,fibDP(n-1,arr)+fibDP(n-2,arr));
+        return arr.get(n);
     }
 }
